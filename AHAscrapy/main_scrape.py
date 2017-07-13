8e3d37ded4950c7d06a1621e461afd337d140870
hautahi <hrk55@cornell.edu>
2017-07-13 10:35:07 -0400

    Tidy up terminal print commands.

--- AHAscrapy/main_scrape.py
+++ AHAscrapy/main_scrape.py
@@ -8,12 +8,13 @@ from urlparse import urlparse
 import requests
 import sys
 import OpenSSL
+import time
 
 #-------------------------------------------------------------#
 # 2. Define Functions
 #-------------------------------------------------------------#
 
-s1, s2 = 0, 4
+s1, s2 = 2, 4
 
 d_name = "../input/hospital_list.csv"
 
@@ -22,22 +23,27 @@ print('Now reading the main input file...')
 d = pd.read_csv(d_name)
 d.columns = map(str.lower, d.columns)
 df = d[s1:s2]
+print("")
 
+# Define user agent
 h = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"}
 
+# Loop over hospitals
+i = s1
 for url, name in zip(df["website"], df["hospital name"]):
 
-    print('Processing hospital: '+name)
-
-	# Check if there's a redirect
+    print('Processing hospital: ' +name+ ', index = %s' %i)
+    
+    start_time = time.time()
+			
+    # Check if there's a redirect
     try:
         response = requests.get(url, timeout=5,headers=h,verify=True)
         if response.url.startswith(url)==False:
 		    url_new = response.url
-		    print("Request was redirected. From: "+url)
+		    print("Request was redirected from: "+url)
 			#print("From: "+url)
-			
-            #print("To: "+url_new)
+                    print("To: "+url_new)
         else:
             url_new = url
     except requests.exceptions.HTTPError:
@@ -51,24 +57,26 @@ for url, name in zip(df["website"], df["hospital name"]):
     except OpenSSL.SSL.Error:
 		url_new = url
 
-    full = "url="+url_new
     print("Scraping: " + url_new)
-    fname = "../output/scrapy_content/"+name+".csv"
-    
-    parsed_url = urlparse(url_new).netloc
-    print(parsed_url)
-    short_url = "al="+parsed_url
-    print(short_url)
 
-	# Remove old file if it exists because scrapy automatically appends
+    # Create the allowed_domain url 
+    parsed_url = urlparse(url_new).netloc
+    short_url = "al="+parsed_url
+    print("allowed domain: " + parsed_url)
+
+    # Remove old file if it exists because scrapy automatically appends
+    fname = "../output/scrapy_content/"+name+".csv"
     try:
         os.remove(fname)
     except OSError:
         pass
 
-	# Call scrapy and suppress output
+    # Call scrapy and suppress output
+    full = "url="+url_new
     FNULL = open(os.devnull,'w')
     retcode = subprocess.call(["scrapy", "crawl", "AHAscrape", "-a", full,"-a",short_url,"-o",fname,"-t","csv"],stdout=FNULL,stderr=subprocess.STDOUT)
 
+    print("--- %s seconds ---" % (time.time() - start_time))
+    i += 1
    # subprocess.call(["scrapy", "crawl", "AHAscrape", "-a", full,"-a",short_url,"-o",fname,"-t","csv"])
 
